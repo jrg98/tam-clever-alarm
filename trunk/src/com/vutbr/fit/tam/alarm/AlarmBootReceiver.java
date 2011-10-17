96093f638a465dce50f6c4df562a8067b1fd4312
VaveRRo@gmail.com <VaveRRo@gmail.com@5dbdea4e-9c19-26ea-f72b-049195e4e478>
2011-10-17 20:08:49 +0000

    pridane subory na zabezpecenie budika - TODO napojenie na kalendar, napojenie na databazu

---
+++ trunk/src/com/vutbr/fit/tam/alarm/AlarmBootReceiver.java
@@ -0,0 +1,15 @@
+package com.vutbr.fit.tam.alarm;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class AlarmBootReceiver extends BroadcastReceiver {
+
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		// pri nabootovani nastavi na poslednu ulozenu hodnotu
+
+	}
+
+}

cdbb3e04a7b54d4dde71905b51d11aa9c080fd31
ondra.lestinsky <ondra.lestinsky@gmail.com>
2011-10-30 16:46:26 +0000

    -- do GUI pridano nastaveni alarmu a sleepmodu -- aktualizace widgetu -- nemusi byt uplne korektni pridavani casu u alarmu, nepochopil jsem hodnoty - nejspis bude potreba mala uprava..

--- src/com/vutbr/fit/tam/activity/DayInfoTab.java
+++ src/com/vutbr/fit/tam/activity/DayInfoTab.java
@@ -3,12 +3,19 @@ package com.vutbr.fit.tam.activity;
 import android.app.TabActivity;
 import android.content.Intent;
 import android.content.res.Resources;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.TabHost;
 import android.widget.TextView;
 import android.widget.TabHost.TabSpec;
 
 import com.vutbr.fit.tam.R;
+import com.vutbr.fit.tam.alarm.Alarm;
+import com.vutbr.fit.tam.database.AlarmAdapter;
 import com.vutbr.fit.tam.gui.Days;
 
 public class DayInfoTab extends TabActivity {
@@ -22,12 +29,14 @@ public class DayInfoTab extends TabActivity {
 	        TabHost tabHost = this.getTabHost();
 	        
 	        Bundle bundle = this.getIntent().getExtras();
+        
 	        
 	        // Tab for day info
 	        TabSpec dayGeneral = tabHost.newTabSpec("Day");
 	        dayGeneral.setIndicator("Day", getResources().getDrawable(R.drawable.tab_day));
 	        Intent dayIntent = new Intent(this, Day.class);
 	        dayIntent.putExtras(bundle);
+	
 	        dayGeneral.setContent(dayIntent);
 	        
 	        // Tab for day alarm
@@ -52,4 +61,30 @@ public class DayInfoTab extends TabActivity {
 	        	        
 	        
 	    }
+	  
+
+		@Override
+	    public boolean onCreateOptionsMenu(Menu menu) {
+	    	MenuInflater inflater = this.getMenuInflater();
+	    	inflater.inflate(R.menu.settings_menu, menu);
+	    	return true;
+	    }
+		
+	    public boolean onOptionsItemSelected(MenuItem item) {
+	    	switch(item.getItemId()) {
+	    		case R.id.button_menu_settings:
+	    			this.showSettings();
+	    			break;
+	    		default:	
+	    			return super.onOptionsItemSelected(item);
+	    	}
+	    	
+	    	return true;
+	    }
+	    
+	    private void showSettings () {
+	    	Intent intent = new Intent(this, Settings.class);
+	    	this.startActivityForResult(intent, 0);
+	    }
+	    
 }

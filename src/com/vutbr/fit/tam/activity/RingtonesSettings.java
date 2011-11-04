4ddb2aad4f02afb213215332fa8bb4cf73584387
jalsovszky <jalsovszky@gmail.com>
2011-11-04 17:30:27 +0000

    Ringing.java: Activity for popup window and ringing. RingtonesSettings.java: Update and button for test ringing.

--- src/com/vutbr/fit/tam/activity/RingtonesSettings.java
+++ src/com/vutbr/fit/tam/activity/RingtonesSettings.java
@@ -22,11 +22,11 @@ import android.widget.TextView;
  */
 public class RingtonesSettings extends Activity implements OnClickListener {
 	
-	private Button stopRingtoneButton;
 	private Button playRingtoneButton;
 	private Button selectRingtoneButton;
 	private Ringtone ringtone;
 	private TextView ringtoneName;
+	private Uri uri;
 	
 	public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,53 +35,39 @@ public class RingtonesSettings extends Activity implements OnClickListener {
         this.playRingtoneButton = (Button) this.findViewById(R.id.playRingtone);
         this.playRingtoneButton.setOnClickListener(this);
         
-        this.stopRingtoneButton = (Button) this.findViewById(R.id.stopRingtone);
-        this.stopRingtoneButton.setOnClickListener(this);
-        
         this.selectRingtoneButton = (Button) this.findViewById(R.id.selectRingtone2);
         this.selectRingtoneButton.setOnClickListener(this);
         
         this.ringtoneName = (TextView) this.findViewById(R.id.settingsRingtoneName);
+        
+        // TODO Load uri from database, default ringtone:
+        uri = Uri.parse("content://settings/system/ringtone");
+        
+        if (uri != null) {
+        	ringtone = RingtoneManager.getRingtone(getApplicationContext(), uri);
+        	ringtoneName.setText(ringtone.getTitle(this));
+        } else {
+        	ringtone = null;
+        	ringtoneName.setText(R.string.no_ringtone);
+        }
     }
 
 	private void selectRingtone() {
-		stopRingtone();
 		Intent intent = new Intent(RingtoneManager.ACTION_RINGTONE_PICKER); 
-		intent.putExtra(RingtoneManager.EXTRA_RINGTONE_TITLE, "Select alarm tone");
+		intent.putExtra(RingtoneManager.EXTRA_RINGTONE_TITLE, "Select alarm ringtone");
 	    startActivityForResult(intent, 1);
-	}
-	
-	private void playRingtone() {
-		if (ringtone != null && ringtone.isPlaying() == false) {
-			ringtone.play();
-			playRingtoneButton.setEnabled(false);
-			stopRingtoneButton.setEnabled(true);
-		} else {
-			
-		}
-	}
-	
-	private void stopRingtone() {
-		if (ringtone != null && ringtone.isPlaying() == true) {
-			ringtone.stop();
-			playRingtoneButton.setEnabled(true);
-			stopRingtoneButton.setEnabled(false);
-		} else {
-			
-		}
 	}
 	
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 		if (resultCode == RESULT_OK) {
-            Uri uri = data.getParcelableExtra(RingtoneManager.EXTRA_RINGTONE_PICKED_URI);
+            uri = data.getParcelableExtra(RingtoneManager.EXTRA_RINGTONE_PICKED_URI);
             if (uri != null) {
                 ringtone = RingtoneManager.getRingtone(getApplicationContext(), uri);
-                ringtoneName.setText(ringtone.getTitle(getApplicationContext()));
-                playRingtoneButton.setEnabled(true);
+                ringtoneName.setText(ringtone.getTitle(this));
+                // TODO Save uri.toString() to database
             } else {
-            	ringtoneName.setText(R.string.not_set);
-            	playRingtoneButton.setEnabled(false);
-                stopRingtoneButton.setEnabled(false);
+            	ringtoneName.setText(R.string.no_ringtone);
+                // TODO No ringtone selected (silence). Delete URI from database.
             }
 }
 	}
@@ -93,10 +79,8 @@ public class RingtonesSettings extends Activity implements OnClickListener {
 				this.selectRingtone();
 				break;
 			case R.id.playRingtone:
-				this.playRingtone();
-				break;
-			case R.id.stopRingtone:
-				this.stopRingtone();
+				Intent intent = new Intent(this, Ringing.class);
+		    	this.startActivityForResult(intent, 0);
 				break;
 			}
 			

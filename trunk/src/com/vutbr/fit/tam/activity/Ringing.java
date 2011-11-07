34d22bb2d9028e1ee486787c1e446f59ae7cb607
jalsovszky@gmail.com <jalsovszky@gmail.com@5dbdea4e-9c19-26ea-f72b-049195e4e478>
2011-11-07 16:17:16 +0000

    AlarmLauncher.java: spusti novu aktivitu Ringing.java - vyzvananie Ringing.java: prebudi telefon, zapne obrazovku a zacne zvonit. Snooze zatial nastavene na 10sec kvoli testovaniu...

--- trunk/src/com/vutbr/fit/tam/activity/Ringing.java
+++ trunk/src/com/vutbr/fit/tam/activity/Ringing.java
@@ -1,24 +1,53 @@
 package com.vutbr.fit.tam.activity;
 
+import java.util.Calendar;
+
 import com.vutbr.fit.tam.R;
+import com.vutbr.fit.tam.alarm.AlarmLauncher;
 
 import android.app.Activity;
+import android.app.AlarmManager;
 import android.app.AlertDialog;
+import android.app.PendingIntent;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.media.Ringtone;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
 
 
-public class Ringing extends Activity {
+public class Ringing extends Activity implements OnClickListener {
 	
 	private Ringtone ringtone;
 	private Uri uri;
+	private Button stopButton;
+	private Button snoozeButton;
 
 	public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //this.setContentView(R.layout.ringing);
+        this.setContentView(R.layout.ringing);
+        
+        final Window win = getWindow();
+        win.addFlags(WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED
+        | WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD);
+        // Turn on the screen unless we are being launched from the AlarmAlert
+        // subclass.
+        if (!getIntent().getBooleanExtra("SCREEN_OFF", false)) {
+        win.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON
+        | WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON);
+        } 
+        
+        this.stopButton = (Button) this.findViewById(R.id.stopButton);
+        this.stopButton.setOnClickListener(this);
+        
+        this.snoozeButton = (Button) this.findViewById(R.id.snoozeButton);
+        this.snoozeButton.setOnClickListener(this);
         
         // TODO Load uri from database, default ringtone:
         uri = Uri.parse("content://settings/system/ringtone");
@@ -26,7 +55,7 @@ public class Ringing extends Activity {
         if (uri != null) {
         	ringtone = RingtoneManager.getRingtone(getApplicationContext(), uri);
         }
-        
+        /*
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setMessage("Wake up you lazy bitch!")
                .setCancelable(false)
@@ -42,25 +71,66 @@ public class Ringing extends Activity {
                         dialog.cancel();
                         stopRingtone();
                         // TODO Schedule next alarm in x minutes
+                        Intent intent = new Intent(Ringing.this, AlarmLauncher.class);
+                        PendingIntent sender = PendingIntent.getBroadcast(Ringing.this,
+                                0, intent, 0);
+                        Calendar calendar = Calendar.getInstance();
+                        calendar.setTimeInMillis(System.currentTimeMillis());
+                        calendar.add(Calendar.SECOND, 70);
+
+                        // Schedule the alarm!
+                        AlarmManager am = (AlarmManager)getSystemService(ALARM_SERVICE);
+                        am.set(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), sender);
                         finish();
                    }
                });
         AlertDialog alert = builder.create();
-        
-        playRingtone();
-        alert.show();
+        */
+        startRinging();
+        /*alert.show();*/
     }
 	
-	private void playRingtone() {
+	private void startRinging() {
 		if (ringtone != null && ringtone.isPlaying() == false) {
 			ringtone.play();
 		}
 	}
 	
-	private void stopRingtone() {
+	private void snooze() {
 		if (ringtone != null && ringtone.isPlaying() == true) {
 			ringtone.stop();
 		}
+		Intent intent = new Intent(Ringing.this, AlarmLauncher.class);
+        PendingIntent sender = PendingIntent.getBroadcast(Ringing.this,
+                0, intent, 0);
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(System.currentTimeMillis());
+        calendar.add(Calendar.SECOND, 10);
+
+        // Schedule the alarm!
+        AlarmManager am = (AlarmManager)getSystemService(ALARM_SERVICE);
+        am.set(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), sender);
+        finish();
+	}
+	
+	private void stopRinging() {
+		if (ringtone != null && ringtone.isPlaying() == true) {
+			ringtone.stop();
+		}
+		finish();
+	}
+
+	public void onClick(View v) {
+		
+		switch (v.getId()) {
+		case R.id.stopButton:
+			this.stopRinging();
+			break;
+		case R.id.snoozeButton:
+			this.snooze();
+			break;
+		}
+		
 	}
 	
 }
--- trunk/src/com/vutbr/fit/tam/alarm/AlarmLauncher.java
+++ trunk/src/com/vutbr/fit/tam/alarm/AlarmLauncher.java
@@ -1,5 +1,7 @@
 package com.vutbr.fit.tam.alarm;
 
+import com.vutbr.fit.tam.activity.Ringing;
+
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -8,8 +10,11 @@ public class AlarmLauncher extends BroadcastReceiver {
 
 	@Override
 	public void onReceive(Context context, Intent intent) {
-		// TODO spusti aktivitu na alarm
-
+		Intent i = new Intent(context, Ringing.class);
+		i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+		i.addFlags(Intent.FLAG_ACTIVITY_BROUGHT_TO_FRONT);
+		i.addFlags(Intent.FLAG_FROM_BACKGROUND);
+    	context.startActivity(i);
 	}
 
 }

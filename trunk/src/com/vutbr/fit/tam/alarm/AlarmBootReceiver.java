e9a95133c2eeeac86969821fa5531f98cf611177
VaveRRo@gmail.com <VaveRRo@gmail.com@5dbdea4e-9c19-26ea-f72b-049195e4e478>
2011-11-17 15:02:21 +0000

    pridany reciever na spustenie sleep modu - POZOR pri spusteni recieveru sa musi VZDY otestovat ci je sleepmod umozneny - tj mrknut do DB ci je alarm povoleny - pretoze sleepmod sa nastavuje na dalsiu hodnotu vzdy ked sa odpali reciever - tj. musi sa odplait vzdy

--- trunk/src/com/vutbr/fit/tam/alarm/AlarmBootReceiver.java
+++ trunk/src/com/vutbr/fit/tam/alarm/AlarmBootReceiver.java
@@ -1,7 +1,10 @@
 package com.vutbr.fit.tam.alarm;
 
+import java.util.Date;
+
 import com.vutbr.fit.tam.calendar.CalendarChecker;
 import com.vutbr.fit.tam.database.AlarmAdapter;
+import com.vutbr.fit.tam.sleep.SleepLauncher;
 
 import android.app.AlarmManager;
 import android.app.PendingIntent;
@@ -27,6 +30,7 @@ public class AlarmBootReceiver extends BroadcastReceiver {
 		// nastavi alarmManager pre kontrolovanie zmien v kalendari
 		this.setCalendarCheckerTime(context, DEFAULT_PERIOD);
 		this.setAlarmTime(context);
+		this.setSleepTime(context);
 
 	}
 	
@@ -66,6 +70,34 @@ public class AlarmBootReceiver extends BroadcastReceiver {
 								actAlarm,
 								pi);
 					}
+				}
+			
+			aD.close();
+			
+		} catch (SQLException ex) {
+			// mozna hlaska o zlyhani nacitania DB
+			Log.e("AlarmBootReciever", "AlarmAdapter error: "+ex.toString());
+			return;
+		}
+	}
+	
+	private void setSleepTime(Context c) {
+		AlarmAdapter aD;
+		long actAlarm;
+		try {
+			aD = new AlarmAdapter(c).open();
+			Cursor cursorACT = aD.fetchAlarm(new Date(System.currentTimeMillis()).getDay());
+			 
+				if (cursorACT.moveToFirst()) {
+					// ak mame nejaky zaznam alarmu, tak zistime ci je potrebne alarm updatovat
+					actAlarm = cursorACT.getLong(3);
+				
+					// nastavi alarm iba ak este nenastal ked bol device vypnuty, inak by ho odpalilo hned
+					AlarmManager mgr=(AlarmManager)c.getSystemService(Context.ALARM_SERVICE);
+					Intent i=new Intent(c, SleepLauncher.class);
+					PendingIntent pi=PendingIntent.getBroadcast(c, 0, i, 0);
+			    
+					mgr.set(AlarmManager.RTC_WAKEUP, actAlarm, pi);
 				}
 			
 			aD.close();

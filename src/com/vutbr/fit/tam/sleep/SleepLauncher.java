4e91e929a64451f7bfcc9738f9e8f432dd16ccaf
jalsovszky <jalsovszky@gmail.com>
2011-11-28 17:30:30 +0000

    Implementovany, no zatial netestovany sleepmode

--- src/com/vutbr/fit/tam/sleep/SleepLauncher.java
+++ src/com/vutbr/fit/tam/sleep/SleepLauncher.java
@@ -5,6 +5,7 @@ import java.util.Date;
 import com.vutbr.fit.tam.alarm.Alarm;
 import com.vutbr.fit.tam.alarm.AlarmLauncher;
 import com.vutbr.fit.tam.database.AlarmAdapter;
+import com.vutbr.fit.tam.database.SettingsAdapter;
 
 import android.app.AlarmManager;
 import android.app.PendingIntent;
@@ -12,20 +13,36 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
+import android.media.AudioManager;
+import android.net.Uri;
 import android.util.Log;
 import android.widget.Toast;
 
 public class SleepLauncher extends BroadcastReceiver {
 
+	private SettingsAdapter settingsAdapter;
+	private AudioManager audioManager;
+	private int isSleepActive;
+	private int sleepRingMode;
 	
 	@Override
 	public void onReceive(Context c, Intent arg1) {
-		// TODO launch sleep routine, if alarm is enabled
+	
+		settingsAdapter = new SettingsAdapter(c);
+		audioManager = (AudioManager) c.getSystemService(Context.AUDIO_SERVICE);
 		
+		settingsAdapter.open();
+		isSleepActive = Integer.parseInt(settingsAdapter.fetchSetting("isSleepActive", "0"));
+		sleepRingMode = Integer.parseInt(settingsAdapter.fetchSetting("sleepRingMode", "0"));
+	
+		if (isSleepActive == 0) {
+			if (!settingsAdapter.updateSetting("sleepRingModeB", Integer.toString(audioManager.getRingerMode())))
+	        	settingsAdapter.insertSetting("sleepRingModeB", Integer.toString(audioManager.getRingerMode()));
+			audioManager.setRingerMode(sleepRingMode);
+			if (!settingsAdapter.updateSetting("isSleepActive", Integer.toString(1)))
+	        	settingsAdapter.insertSetting("isSleepActive", Integer.toString(1));
+		}
 		
-
-
+		this.settingsAdapter.close();
 	}
-
-
 }
